6b9629482eeacd5cb7d77336c9a274e6b1234184
Jason Karns <jason.karns@gmail.com>
2020-03-18 13:40:43 -0400

    node-version now depends on setup-node directly

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const core = require('@actions/core')
 const { installNodenv } = require('../src/installer')
 
-async function run () {
+function run () {
   return Promise.resolve()
     .then(() => installNodenv(core.getInput('nodenv-version')))
-    .catch(error => core.setFailed(error.message))
+    .catch(err => core.setFailed(err.message))
 }
 
-run()
+module.exports = run()
